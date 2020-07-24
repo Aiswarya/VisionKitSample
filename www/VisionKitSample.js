ac70d4e6367fe6b6c3e9fdd4fc1ccbc96a8b67dd
Aiswarya <aiswaryakannan@gmail.com>
2020-07-24 15:01:09 +0530

    Updated comments

--- www/VisionKitSample.js
+++ www/VisionKitSample.js
@@ -3,9 +3,9 @@ var exec = require('cordova/exec');
 /**
  * Scan and retreive line occurence of the given text.
  *
- * @param text Text whose lines to be is dentified in a page
- * @param success success callback which contains the array of lines.
- * @param error error callback with error message.
+ * @param text Text whose lines to be identified in a page
+ * @param success Success callback which contains the array of lines.
+ * @param error Error callback with error message.
  */
 exports.scanAndRetreiveLines = function (text,success, error) {
     exec(success, error, 'VisionKitSample', 'scanAndRetreiveLines', [text]);
