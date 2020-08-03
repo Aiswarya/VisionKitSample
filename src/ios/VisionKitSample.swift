5717eb7b3951df1e5f24a97707bd6120170b75fc
Aiswarya K <>
2020-08-03 13:27:12 +0530

    1. Access specifiers added. 2. callback logic issue fixed. 	Invoking the plugin method when a processing in progress issue will sent error response. 3. searchText saving issue fixed. 	guard let is added to make sure there is string reference to self. 4. Removed '!'. 5. Documented the method -  'scanAndRetreiveLines'. 6. Moved the input text check to javascript.

--- src/ios/VisionKitSample.swift
+++ src/ios/VisionKitSample.swift
@@ -4,131 +4,176 @@ import Foundation
 import Vision
 import VisionKit
 
-let INVALIDINPUTERROR = "Invalid input. Please check whether valid input is passed to the plugin"
-let TEXTNOTAVAILABLERROR = "Text doesn't have a suitable match in the scanned page"
-let SCANNINGFAILEDERROR = "Scanning failed"
-let CANCELLEDERROR = "Scanning is cancelled"
-let SCANNERNOTAVAILABLE = "Scanner is not available"
-let PROCESSFAILURE = "Text processing failed"
-let LINESIDENTIFIED_KEY = "linesIdentified"
+fileprivate struct Constants {
+    static let INVALIDINPUTERROR = "Invalid input. Please check whether valid input is passed to the plugin"
+    static let TEXTNOTAVAILABLERROR = "Text doesn't have a suitable match in the scanned page"
+    static let SCANNINGFAILEDERROR = "Scanning failed"
+    static let CANCELLEDERROR = "Scanning is cancelled"
+    static let SCANNERNOTAVAILABLE = "Scanner is not available"
+    static let PROCESSFAILURE = "Text processing failed"
+    static let LINESIDENTIFIED_KEY = "linesIdentified"
+    static let INPROGRESSERROR = "Previous text processing is in progress"
+}
 
-///@objc is added to expose the class to Objective C classes
-@objc(VisionKitSample) class VisionKitSample: CDVPlugin {
+//@objc is added to expose the class to Objective C classes
+@objc(VisionKitSample) public class VisionKitSample: CDVPlugin {
+    private var callbackId: String?
+    private var searchText: String?
     
-    var callbackId: String?
-    var searchText: String?
+    /**
+    Function that wil be called from JS to retreive the lines of a given text by scanning a page.
     
-    ///@objc is added to expose the function  to Objective C classes
-    ///Function that will be called from JS to scan a page and identify the lines that contain the text that was passed as input
+    All the text validations are performed in the JS side and only valid input is passed to this function.
+    
+    `Callback`
+    - callbackId is saved for responding back to JS from delegate methods.
+    - Text processing is performed asynchronously.
+    - When a processing is in progress and if this method gets invoked again with new callbackId,
+    error indicating an existing process will be sent back to JS .
+    
+    `Camera view`
+    - Once searchText is extracted, VNDocumentCameraViewController is presented for scanning a page.
+    - User permissions are automatically handled in the VNDocumentCameraViewController and its delegate
+    methods are called based on user actions.
+    
+    - Parameter command: Input from JS with callbacks and arguments.
+    */
     @objc(scanAndRetreiveLines:)
-    func scanAndRetreiveLines(command: CDVInvokedUrlCommand) {
+    public func scanAndRetreiveLines(command: CDVInvokedUrlCommand) {
+        if let _ = callbackId{
+            self.sendFailureMessage(message: Constants.INPROGRESSERROR, callbackId: command.callbackId)
+            return
+        }else{
+            self.callbackId = command.callbackId
+        }
         
-        ///callbackId is saved for sending the responses  back to JS from other functions using the same callbackId
-        self.callbackId = command.callbackId
-        
-        ///VisionKit is available only from iOS 13, error message is returned if this is executed in a lower version
+        //VisionKit is available only from iOS 13, error message is returned if this is executed in a lower version
         if #available(iOS 13.0, *) {
-            ///Convert the passed parameter to String representation.
-            ///If the String conversion succeeds, VNDocumentCameraViewController() will be presented
-            ///enabling the user to scan a page by providing camera permission
-            ///If the string conversion is nil or string contains only white spaces or new line characters, then error message is returned
-            
-            self.commandDelegate.run {[weak self] in
-                let inputReceived = String(describing: command.argument(at: 0) ?? "")
-                if (inputReceived.isEmpty || (inputReceived.trimmingCharacters(in: .whitespacesAndNewlines) == "")) {
-                    self?.sendFailureMessage(message: INVALIDINPUTERROR)
-                    return
-                }
-                self?.searchText = inputReceived
-                DispatchQueue.main.async {
-                    let scanVC = VNDocumentCameraViewController()
-                    scanVC.delegate = self
-                    self?.viewController.present(scanVC, animated: true)
-                }
+            if let inputText = command.argument(at: 0) as? String{
+                self.searchText = inputText
+                let scanVC = VNDocumentCameraViewController()
+                scanVC.delegate = self
+                self.viewController.present(scanVC, animated: true)
+            }else{
+                self.sendFailureMessage(message: Constants.INVALIDINPUTERROR, callbackId: command.callbackId)
             }
         }else{
-            self.sendFailureMessage(message: SCANNERNOTAVAILABLE)
+            self.sendFailureMessage(message: Constants.SCANNERNOTAVAILABLE, callbackId: command.callbackId)
         }
     }
     
+    ///Process the scanned image.
+    ///- Parameter image: scanned image to be processed.
     @available(iOS 13.0, *)
-    ///Function to initialize the processing of the scanned image
-    ///@param image to be processed
-    func processScannedImage(_ image: UIImage) {
+    private func processScannedImage(_ image: UIImage) {
         guard let cgImage = image.cgImage else { return }
         
         let requestHandler = VNImageRequestHandler(cgImage: cgImage)
         let request = VNRecognizeTextRequest(completionHandler: recognizeTextHandler)
         do {
-            ///Process the text recognize request and pass the result to the completion handler
+            //Process the text recognize request and pass the result to the completion handler
             try requestHandler.perform([request])
         } catch {
-            sendFailureMessage(message: PROCESSFAILURE)
+            if let callbackId = self.callbackId {
+                sendFailureMessage(message: Constants.PROCESSFAILURE, callbackId: callbackId)
+            }
         }
     }
     
+    ///Retreive the lines from the observations result of the scanned page processing.
+    ///- Parameter request: processed request that contains the result from which observations can be extracted.
+    ///- Parameter error: error occured during the image processing.
     @available(iOS 13.0, *)
-    ///Function to recognize the strings from the observation result and returns back the lines which contain the text passed from JS
-    func recognizeTextHandler(request: VNRequest, error: Error?) {
+    private func recognizeTextHandler(request: VNRequest, error: Error?) {
+        guard let callbackId = self.callbackId else { return }
+        
+        guard let searchText = self.searchText else {
+            sendFailureMessage(message: Constants.INVALIDINPUTERROR, callbackId: callbackId)
+            return
+        }
+        
         guard let observations =
             request.results as? [VNRecognizedTextObservation] else {
+                sendFailureMessage(message: Constants.TEXTNOTAVAILABLERROR, callbackId: callbackId)
                 return
         }
         
         var recognizedStrings = observations.compactMap { observation in
-            ///Return the string of the top VNRecognizedText instance.
+            //Return the string of the top VNRecognizedText instance.
             return observation.topCandidates(1).first?.string
         }
         
-        recognizedStrings = recognizedStrings.filter{$0.lowercased().contains(self.searchText!.lowercased())}
+        recognizedStrings = recognizedStrings.filter{$0.lowercased().contains(searchText.lowercased())}
         if recognizedStrings.count == 0 {
-            sendFailureMessage(message: TEXTNOTAVAILABLERROR)
+            sendFailureMessage(message: Constants.TEXTNOTAVAILABLERROR, callbackId: callbackId)
         }else{
-            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: [LINESIDENTIFIED_KEY: recognizedStrings])
-            self.commandDelegate!.send(pluginResult, callbackId: self.callbackId!);
+            self.sendSuccessMessage(withLines: recognizedStrings, callbackId: callbackId)
         }
     }
     
-    ///Function to sent error responses
-    ///@param message to be sent as plugin response
-    func sendFailureMessage(message: String) {
+    ///Send error responses back to JS and clear self.callbackId to accept new callback.
+    ///- Parameter message: message to be passed as response.
+    ///- Parameter callbackId:callbackId to which plugin result should be sent.
+    private func sendFailureMessage(message: String, callbackId: String) {
         let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: message)
-        self.commandDelegate!.send(pluginResult, callbackId: self.callbackId!);
+        self.commandDelegate.send(pluginResult, callbackId: callbackId);
+        if callbackId == self.callbackId{
+            self.callbackId = nil
+        }
+    }
+    
+    ///Send success response back to JS along with the recognized lines of the text passed as input to the plugin and clear self.callbackId to accept new callback.
+    ///- Parameter lines: Lines that contain the text.
+    ///- Parameter callbackId: callbackId to which plugin result should be sent.
+    private func sendSuccessMessage(withLines lines: [String], callbackId: String) {
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: [Constants.LINESIDENTIFIED_KEY: lines])
+        self.commandDelegate.send(pluginResult, callbackId: callbackId);
+        if callbackId == self.callbackId{
+            self.callbackId = nil
+        }
     }
 }
 
 @available(iOS 13.0, *)
 extension VisionKitSample: VNDocumentCameraViewControllerDelegate {
     
-    ///Function gets called when user clicks Save after successfully scanning the pages
-    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
-        ///If the scanned page count is zero, error response will be sent back to JS
+    //Function gets called when user clicks Save after successfully scanning the page.
+    public func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+        //If the scanned page count is zero, error response will be sent back to JS.
         guard scan.pageCount >= 1 else {
-            sendFailureMessage(message: SCANNINGFAILEDERROR)
+            if let callbackId = self.callbackId {
+                sendFailureMessage(message: Constants.SCANNINGFAILEDERROR, callbackId: callbackId)
+            }
             controller.dismiss(animated: true)
             return
         }
         
-        ///This plugin will be processing the text in a single page scanned by the user,
-        ///if there are more than 1 page scanned, only the first page will be used for processing.
-        ///Processing of the image is executed in background, so that the screen is not hanged during the processing of the scanned page
+        //This plugin will be processing the text in a single page scanned by the user,
+        //if there are more than 1 page scanned, only the first page will be used for processing.
+        //Processing of the image is executed in background, so that the screen is not hanged during the processing of the scanned page.
         self.commandDelegate.run {[weak self] in
-            self?.processScannedImage(scan.imageOfPage(at: 0))
+            guard let self = self else { return }
+            
+            self.processScannedImage(scan.imageOfPage(at: 0))
         }
         
         controller.dismiss(animated: true)
     }
     
-    ///Function gets called  when any failure happens while scanning or when user clicks Cancel after denying the camera permission
-    ///Error response will be sent back to JS
-    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
-        sendFailureMessage(message: SCANNINGFAILEDERROR)
+    //Function gets called when any failure happens while scanning or when user clicks Cancel after denying the camera permission.
+    //Error response will be sent back to JS.
+    public func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
+        if let callbackId = self.callbackId {
+            sendFailureMessage(message: Constants.SCANNINGFAILEDERROR, callbackId: callbackId)
+        }
         controller.dismiss(animated: true)
     }
     
-    ///Function gets called when user clicks Cancel, error response will be sent back to JS
-    func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
-        sendFailureMessage(message: CANCELLEDERROR)
+    //Function gets called when user clicks Cancel, error response will be sent back to JS.
+    public func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
+        if let callbackId = self.callbackId {
+            sendFailureMessage(message: Constants.CANCELLEDERROR, callbackId: callbackId)
+        }
         controller.dismiss(animated: true)
     }
 }
--- www/VisionKitSample.js
+++ www/VisionKitSample.js
@@ -7,6 +7,25 @@ var exec = require('cordova/exec');
  * @param success Success callback which contains the array of lines.
  * @param error Error callback with error message.
  */
-exports.scanAndRetreiveLines = function (text,success, error) {
-    exec(success, error, 'VisionKitSample', 'scanAndRetreiveLines', [text]);
+exports.scanAndRetreiveLines = function (success, error, text) {
+    if (typeof success != "function") {
+        console.log("success must be of type function.");
+        return
+    }
+    if (typeof error != "function") {
+        console.log("error must be of type function.");
+        return
+    }
+    try {
+        //Error callback is invoked if string validation fails
+        var inputText = text.toString();
+        if (inputText.trim() == "" || /\n/.test(inputText)){
+            error("Please make sure the input is neither empty nor contains newline character.");
+        }else{
+            exec(success, error, 'VisionKitSample', 'scanAndRetreiveLines', [inputText]);
+        }
+    }
+    catch(err) {
+        error("Invalid string.");
+    }
 };
